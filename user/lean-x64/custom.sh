a76cf439947ce852829a326d16ab238d7e486d31
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2022-04-05 22:42:19 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -6,7 +6,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06            
 rm -rf feeds/packages/libs/libcap
 svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap             feeds/packages/libs/libcap
 git clone https://github.com/pymumu/luci-app-smartdns.git -b lede               package/applications/luci-app-smartdns
-git clonehttps://github.com/tty228/luci-app-serverchan.git                      package/applications/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git                      package/applications/luci-app-serverchan
 git clone https://github.com/destan19/OpenAppFilter.git                         package/OpenAppFilter
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages         package/passwall-packages
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci             package/passwall
