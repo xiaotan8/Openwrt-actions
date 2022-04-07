91d3f2a833213dccbfe5160437a6c025dfaecb0a
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2022-04-08 05:34:19 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Test custom.sh"
+rm -rf  feeds/luci/themes/luci-theme-argon
 git clone  https://github.com/fw876/helloworld.git                              package/helloworld
-git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06            package/luci-theme-argon-jerrykuku
+git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06            feeds/luci/themes/luci-theme-argon
 rm -rf feeds/packages/libs/libcap
 svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap             feeds/packages/libs/libcap
 git clone https://github.com/pymumu/luci-app-smartdns.git -b lede               package/applications/luci-app-smartdns
