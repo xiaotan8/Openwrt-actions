06483c011f5bf237d442d54557d62bbe974e3776
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2022-04-07 21:12:57 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Test custom.sh"
+git clone https://github.com/jerrykuku/luci-theme-argon.git       package/luci-theme-argon-jerrykuku
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
 git clone https://github.com/destan19/OpenAppFilter.git           package/diy/OpenAppFilter
