3fb64f1f15715b9acc4bd6cfa092ae0b3be41670
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2022-06-13 19:51:49 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -11,6 +11,6 @@ git clone https://github.com/pymumu/luci-app-smartdns.git -b lede               
 git clone https://github.com/tty228/luci-app-serverchan.git                     package/applications/luci-app-serverchan
 svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome            package/applications/luci-app-adguardhome
 git clone https://github.com/destan19/OpenAppFilter.git                         package/OpenAppFilter
-# git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages         package/passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages         package/passwall-packages
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci             package/passwall
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash          package/luci-app-openclash
