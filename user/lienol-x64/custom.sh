9a8b609305c3cd0aaf7974b7edd45095a3ce5d1f
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2022-09-23 00:39:00 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -9,5 +9,5 @@ git clone https://github.com/destan19/OpenAppFilter.git           package/diy/Op
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages      package/diy/passwall-packages
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci     package/diy/passwall
 # git clone  https://github.com/fw876/helloworld.git                package/helloworld
-# rm -rf ./target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch
-# wget -P ./target/linux/generic/hack-5.10/ https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch
+rm -rf ./target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch
+wget -P ./target/linux/generic/hack-5.10/ https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch
