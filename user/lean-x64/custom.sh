18707ffc3d43931b5af2a5110a24e22ecedf5cad
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2023-02-02 04:16:37 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -2,7 +2,7 @@
 
 echo "Test custom.sh"
 # rm -rf feeds/luci/themes/luci-theme-argon
-# rm -rf feeds/packages/net/smartdns/Makefile
+rm -rf feeds/packages/net/smartdns/Makefile
 svn co https://github.com/pymumu/openwrt-smartdns/trunk/                  feeds/packages/net/smartdns/
 # git clone https://github.com/fw876/helloworld.git                               package/helloworld
 # git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06          feeds/luci/themes/luci-theme-argontyfreedom
