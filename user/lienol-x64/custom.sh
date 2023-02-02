1622e3d0b38229c0e107a965098b079ead07b430
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2023-02-02 14:32:57 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -3,11 +3,11 @@
 echo "Test custom.sh"
 # rm -rf feeds/luci/themes/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-theme-argon.git       feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
 git clone https://github.com/destan19/OpenAppFilter.git           package/diy/OpenAppFilter
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages      package/diy/passwall-packages
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci     package/diy/passwall
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash          package/luci-app-openclash
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash          package/luci-app-openclash
 # git clone  https://github.com/fw876/helloworld.git                package/helloworld
 
