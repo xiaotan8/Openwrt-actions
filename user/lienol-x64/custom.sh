5138eed435038b86430c49a8db5dace4c48d964b
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2023-09-16 01:20:26 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -8,7 +8,7 @@ git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-a
 git clone https://github.com/destan19/OpenAppFilter.git           package/diy/OpenAppFilter
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages.git      package/diy/passwall-packages
 # git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci-smartdns-dev    package/diy/passwall
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash          package/luci-app-openclash
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash          package/luci-app-openclash
 # git clone  https://github.com/fw876/helloworld.git                package/helloworld
 # svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-samba   feeds/luci/applications/luci-app-samba
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git                    package/luci-app-tcpdump
