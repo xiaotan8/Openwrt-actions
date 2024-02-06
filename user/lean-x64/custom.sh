f5203410d41bfcf1cd9fb8f7d4299df94c007249
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2024-02-06 11:47:29 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -6,11 +6,11 @@ rm -rf feeds/packages/net/smartdns/Makefile
 git clone https://github.com/pymumu/openwrt-smartdns.git                        feeds/packages/net/smartdns/
 # git clone https://github.com/fw876/helloworld.git                             package/helloworld
 # git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06          feeds/luci/themes/luci-theme-argontyfreedom
-rm -rf feeds/packages/libs/libcap
-svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap             feeds/packages/libs/libcap
+#rm -rf feeds/packages/libs/libcap
+#svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap             feeds/packages/libs/libcap
 git clone https://github.com/pymumu/luci-app-smartdns.git -b lede               package/applications/luci-app-smartdns
 git clone https://github.com/tty228/luci-app-serverchan.git                     package/applications/luci-app-serverchan
-svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome            package/applications/luci-app-adguardhome
+#svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome            package/applications/luci-app-adguardhome
 git clone https://github.com/destan19/OpenAppFilter.git                         package/OpenAppFilter
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages.git      package/passwall-packages
 # git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci-smartdns-dev       package/passwall
