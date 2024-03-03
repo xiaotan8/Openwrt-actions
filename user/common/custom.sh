ef555ab87c00df2abc94bbd41bb8e04b8243fbac
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2024-03-03 14:02:20 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 echo "Apply custom.sh"
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
