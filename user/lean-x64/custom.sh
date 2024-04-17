b1cecb770ae6860e965b55e06876b2f2b13e2c14
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2024-04-17 23:06:22 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -4,7 +4,7 @@ echo "Test custom.sh"
 #rm -rf feeds/packages/net/smartdns/Makefile
 #git clone https://github.com/pymumu/openwrt-smartdns.git                        feeds/packages/net/smartdns/
 #git clone https://github.com/pymumu/luci-app-smartdns.git -b lede               package/applications/luci-app-smartdns
-#git clone https://github.com/tty228/luci-app-wechatpush.git -b openwrt-18.06    package/applications/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-wechatpush.git     package/applications/luci-app-wechatpush
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git            package/applications/luci-app-adguardhome
 git clone https://github.com/destan19/OpenAppFilter.git                         package/OpenAppFilter
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git                    package/luci-app-tcpdump
