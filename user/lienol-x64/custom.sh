7a0a94fbed2f427ebba46d15e352601a759e1dbe
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-08-05 13:31:15 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "Test custom.sh"
-# rm -rf feeds/luci/themes/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-theme-argon.git       feeds/luci/themes/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git       feeds/luci/themes/luci-theme-argon
 git clone https://github.com/tty228/luci-app-wechatpush.git       package/luci-app-wechatpush
 git clone https://github.com/destan19/OpenAppFilter.git           package/diy/OpenAppFilter
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git      package/diy/passwall-packages
-# git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci-smartdns-dev    package/diy/passwall
-# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash          package/luci-app-openclash
-# git clone  https://github.com/fw876/helloworld.git                package/helloworld
-# svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-samba   feeds/luci/applications/luci-app-samba
-# git clone https://github.com/KFERMercer/luci-app-tcpdump.git                    package/luci-app-tcpdump
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+git clone https://github.com/vernesong/OpenClash -b master package/openclash
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
