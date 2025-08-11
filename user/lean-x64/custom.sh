f3b58f58ad5235a0051f765169a8c96d08e6b201
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-08-11 23:26:15 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -4,7 +4,10 @@ echo "Test custom.sh"
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-OpenClash
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+git clone https://github.com/vernesong/OpenClash -b master package/openclash
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
 rm -rf feeds/packages/net/smartdns
