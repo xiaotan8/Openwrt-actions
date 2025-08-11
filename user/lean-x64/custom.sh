2b4fafb5a00aa1f4b1f0d8831ee8b502fba45ddf
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-08-12 01:09:54 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -5,6 +5,7 @@ rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2sock
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-nikki
 rm -rf feeds/luci/applications/luci-app-OpenClash
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 git clone https://github.com/vernesong/OpenClash -b master package/openclash
