ac8ac922868382f33c5bf4cef56187ae218fe838
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-08-12 22:35:45 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -5,7 +5,7 @@ rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2sock
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
-rm -rf feeds/luci/applications/luci-app-nikki
+# rm -rf feeds/luci/applications/luci-app-nikki
 rm -rf feeds/luci/applications/luci-app-OpenClash
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 git clone https://github.com/vernesong/OpenClash -b master package/openclash
@@ -19,4 +19,4 @@ git clone https://github.com/tty228/luci-app-wechatpush.git     package/applicat
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git         package/applications/luci-app-adguardhome
 #git clone https://github.com/destan19/OpenAppFilter.git                     package/applications/OpenAppFilter
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git                 package/applications/luci-app-tcpdump
-git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git                package/applications/OpenWrt-nikki
+# git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git                package/applications/OpenWrt-nikki
