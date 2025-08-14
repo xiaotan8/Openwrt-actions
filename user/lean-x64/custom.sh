c58d14e4f30d893232278838ebab472bace097e3
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-08-14 13:27:51 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Test custom.sh"
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/packages/net/{smartdns,xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
@@ -12,7 +12,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 git clone https://github.com/vernesong/OpenClash -b master package/openclash
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-rm -rf feeds/packages/net/smartdns
+# rm -rf feeds/packages/net/smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git                     feeds/packages/net/smartdns/
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git              package/applications/luci-app-smartdns
