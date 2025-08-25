fb7eb6f78ec45ff304688fa401c158ed506be1f7
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-08-25 13:09:41 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -4,12 +4,9 @@ echo "Test custom.sh"
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git       feeds/luci/themes/luci-theme-argon
 git clone https://github.com/tty228/luci-app-wechatpush.git       package/luci-app-wechatpush
-git clone https://github.com/destan19/OpenAppFilter.git           package/diy/OpenAppFilter
+git clone --depth=1  https://github.com/destan19/OpenAppFilter.git           package/diy/OpenAppFilter
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
-git clone https://github.com/vernesong/OpenClash -b master package/openclash
-
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+git clone --depth=1 https://github.com/vernesong/OpenClash.git -b master package/openclash
