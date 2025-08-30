94ee61cc6b16faf29fc9493f7414d89cfd3e2262
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-08-31 00:08:40 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -2,3 +2,4 @@
 
 echo "Test custom.sh"
 git clone --depth=1 https://github.com/xiaotan8/luci-app-accesscontrol.git package/applications/luci-app-accesscontrol
+sed -i 's/PKG_HASH:=150019a03f1ec2e4b5849740a72badf5ea094d5754bd59dd30119523a3ce9398/PKG_HASH:=abcb3d3bfa99297dfb92b8fb4f1f78d0948a01281fdfc76c9c460a2c3d5c7f79/' feeds/packages/net/smartdns/Makefile
