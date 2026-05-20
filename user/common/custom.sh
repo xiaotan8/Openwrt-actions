ec473913351c6c38a4a4bfdde9cd4390b243b411
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2026-05-20 19:20:02 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Apply custom.sh"
-rm -rf feeds/packages/net/{nikki,xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/packages/net/{nikki,xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-passwall2,luci-app-nikk,luci-app-OpenClash,luci-app-wechatpush,luci-app-argon-config,luci-app-argon-momo}
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
